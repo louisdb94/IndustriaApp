1b8bfe06b949357d403924dc6aed7158b943175c
louisdb94 <dalbosco.louis@gmail.com>
2017-11-06 12:40:34 +0100

    merge

--- server/models/student.ts
+++ server/models/student.ts
@@ -1,15 +1,10 @@
 import * as mongoose from 'mongoose';
 
-<<<<<<< HEAD
-const skill = new mongoose.Schema({
-  name : String,
-  value : Number
-=======
 const experienceSchema = new mongoose.Schema({
   function: Array,
   description: Array,
   period: Array
->>>>>>> master
+
 })
 
 const studentSchema = new mongoose.Schema({
@@ -18,14 +13,10 @@ const studentSchema = new mongoose.Schema({
   whoami: { type: String, default: "Elon Musk" },
   degree: String,
   gradYear: Number,
-<<<<<<< HEAD
-  //whoami: String,
-  skills: [mongoose.Schema.Types.Mixed]
-=======
   skills: Array,
   skillsValue: [Number],
   experiences: []
->>>>>>> master
+
 });
 
 const Student = mongoose.model('Student', studentSchema);
