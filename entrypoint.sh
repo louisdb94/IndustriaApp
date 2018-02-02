a25cc68a87fbd10e99f30e04f13425e3abe6fc0f
louisdb94 <dalbosco.louis@gmail.com>
2018-02-02 18:08:56 +0100

    deletions

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,59 +22,6 @@ src="/templates/vhost.sample.conf"
 
 cat "$src" > "$dest"
 
-envsubst '\$APP_URL' < "$src" > "$dest"
-
-#prepare the letsencrypt command arguments
-letscmd="-d $APP_URL "
-
-# # Check if the SAN list has changed
-if [ ! -f /etc/letsencrypt/san_list ]; then
- cat <<EOF >/etc/letsencrypt/san_list
- "${APP_URL}"
-EOF
-  fresh=true
-else 
-  old_san=$(cat /etc/letsencrypt/san_list)
-  if [ "${APP_URL}" != "${old_san}" ]; then
-    fresh=true
-  else 
-    fresh=false
-  fi
-fi
-
-# Initial certificate request, but skip if cached
-if [ $fresh = true ]; then
-  echo "The SAN list has changed, removing the old certificate and ask for a new one."
-  rm -rf /etc/letsencrypt/{live,archive,keys,renewal}
- 
- echo "certbot certonly --preferred-challenges http "${letscmd}" \
-  --standalone --text \  
-  --expand " > /etc/nginx/lets
-  
-  echo "Running initial certificate request... "
-
-  /bin/bash /etc/nginx/lets
-fi
-
-#update the stored SAN list
-echo "${APP_URL}" > /etc/letsencrypt/san_list
-
-#Create the renewal directory (containing well-known challenges)
-mkdir -p /etc/letsencrypt/webrootauth/
-
-# Template a cronjob to renew certificate with the webroot authenticator
-echo "Creating a cron job to keep the certificate updated"
-  cat <<EOF >/etc/periodic/weekly/renew
-#!/bin/sh
-# First renew certificate, then reload nginx config
-certbot renew --webroot --webroot-path /etc/letsencrypt/webrootauth/ --post-hook "/usr/sbin/nginx -s reload"
-EOF
-
-chmod +x /etc/periodic/weekly/renew
-
-# Kick off cron to reissue certificates as required
-# Background the process and log to stderr
-/usr/sbin/crond -f -d 8 &
 
 # Launch nginx in the foreground
 /usr/sbin/nginx -g "daemon off;"
@@ -84,4 +31,3 @@ npm run start
 
 
 echo "Ready"
-
