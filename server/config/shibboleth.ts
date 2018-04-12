50bdc38b0c0bdc2b39a6c57ef55ade2a6db5aca9
louisdb94 <dalbosco.louis@gmail.com>
2018-04-12 19:26:23 +0200

    ppuhsh

--- server/config/shibboleth.ts
+++ server/config/shibboleth.ts
@@ -78,7 +78,7 @@ router.route('/login').get(function(req,res){
 // Assert endpoint for when login completes
 router.route('/assert').get(function(req,res){
 
-    var rnumber = 'r0696969';
+    var rnumber = 'r0707070';
     checkStudent(rnumber, res);
   //res.redirect('http://localhost:4200/home-students/'+ token);
 
