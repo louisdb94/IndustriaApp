f39b11b1323f3b173087f6bde23dde485d67662f
robincroon95 <robin0croon@gmail.com>
2018-04-13 17:11:40 +0200

    push

--- server/controllers/students/students.controller.ts
+++ server/controllers/students/students.controller.ts
@@ -25,21 +25,42 @@ export class StudentsController extends DefaultController {
 
     // this.delete('professional', 'student_fk', req.params.student_fk);
     // this.delete('skills', 'student_fk', req.params.student_fk);
+    
 
     var crud_controller = this.model + "Crud";
-    this[crud_controller].delete('contact', 'id', req.params.student_fk).then(result => {
-      this[crud_controller].delete('cvs', 'id', req.params.student_fk).then(result => {
-        this[crud_controller].delete('privacylog', 'id', req.params.student_fk).then(result => {
-          this[crud_controller].delete('education', 'id', req.params.student_fk).then(result => {
-            this[crud_controller].delete('experiences', 'id', req.params.student_fk).then(result => {
-              this[crud_controller].delete('language', 'id', req.params.student_fk).then(result => {
-                this[crud_controller].delete('professional', 'id', req.params.student_fk).then(result => {
-                  this[crud_controller].delete('skills', 'id', req.params.student_fk).then(result => {
-                    this[crud_controller].delete('socialmedia', 'student_fk', req.params.student_fk).then(result => {
-                      this[crud_controller].delete('students', 'id', req.params.student_fk).then(result => {
-                        res.status(200).json(result );
+
+    this[crud_controller].delete('contact', 'student_fk', req.params.student_fk).then(result => {
+      console.log("contact verwijderd");
+      this[crud_controller].delete('cvs', 'student_fk', req.params.student_fk).then(result => {
+        console.log("cvs verwijderd");
+        this[crud_controller].delete('privacylog', 'student_fk', req.params.student_fk).then(result => {
+          console.log("privacylog verwijderd");
+          this[crud_controller].delete('education', 'student_fk', req.params.student_fk).then(result => {
+            console.log("education verwijderd");
+            this[crud_controller].delete('experiences', 'student_fk', req.params.student_fk).then(result => {
+              console.log("experiences verwijderd");
+              this[crud_controller].delete('language', 'student_fk', req.params.student_fk).then(result => {
+                console.log("language verwijderd");
+                this[crud_controller].delete('professional', 'student_fk', req.params.student_fk).then(result => {
+                  console.log("professional verwijderd");
+                  this[crud_controller].delete('skills', 'student_fk', req.params.student_fk).then(result => {
+                    console.log("skills verwijderd");
+
+                    this[crud_controller].delete('socialmedia', 'student_fk', req.params.student_fk).then(result => { 
+                      console.log("socialmedia verwijderd");
+                      this[crud_controller].getBy('students', 'id', req.params.student_fk).then(result => {
+                        console.log("user_fk: ", result[0].user_fk);
+                        let user_id = result[0].user_fk
+                        this[crud_controller].delete('students', 'id', req.params.student_fk).then(result => {
+                          console.log("student verwijderd");
+                          res.status(200).json(result );
+
+                          // this[crud_controller].delete('user', 'id', user_id).then(result => {
+                          // });
+                        });
                       });
                     });
+
                   });
                 });
               });
@@ -61,12 +82,12 @@ export class StudentsController extends DefaultController {
       });
   }
 
-  deleteStudentTable(res, req, table, name, field){
-    var crud_controller = this.model + "Crud";
-    this[crud_controller].getBy(table, name, field).then(result => {
-      this.delete('students', 'id', req.params.student_fk);
-      //this.delete('user', 'id', result[0].user_fk);
-      res.send("gelukt");
-      });
-  }
+  // deleteStudentTable(res, req, table, name, field){
+  //   var crud_controller = this.model + "Crud";
+  //   this[crud_controller].getBy(table, name, field).then(result => {
+  //     this.delete('students', 'id', req.params.student_fk);
+  //     //this.delete('user', 'id', result[0].user_fk);
+  //     res.send("gelukt");
+  //   });
+  // }
 }
